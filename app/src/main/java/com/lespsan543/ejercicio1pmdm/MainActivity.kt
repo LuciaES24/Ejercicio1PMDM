026c44a33773e647934853e2a3872032c456561d
LuciaES24 <lespsan543@g.educaand.es>
2023-09-26 12:41:06 +0200

    Primera versión Actividad01

--- app/src/main/java/com/lespsan543/ejercicio1pmdm/MainActivity.kt
+++ app/src/main/java/com/lespsan543/ejercicio1pmdm/MainActivity.kt
@@ -2,10 +2,41 @@ package com.lespsan543.ejercicio1pmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var contaPulsaciones = 0
+        val texto : TextView = findViewById(R.id.textoMostrar)
+        val boton : Button = findViewById(R.id.boton_aceptar)
+
+        fun pulsar(){
+            contaPulsaciones++
+            var tamano = 0
+            if (contaPulsaciones<=5){
+                tamano = 24
+            }else{
+                tamano = 22
+            }
+            texto.textSize = tamano.toFloat()
+
+            if (contaPulsaciones == 1){
+                texto.text = "Has hecho click una vez!"
+            }else if(contaPulsaciones == 2){
+                texto.text = "Has hecho click dos veces!"
+            }else if(contaPulsaciones>=3 && contaPulsaciones<=5){
+                texto.text = "Has hecho click $contaPulsaciones veces!"
+            }else if(contaPulsaciones>=6 && contaPulsaciones <=9){
+                texto.text = "Has hecho click varias veces! ($contaPulsaciones)"
+            }else {
+                texto.text = "Te has pasado de clicks!"
+                boton.setEnabled(false)
+            }
+        }
+        boton.setOnClickListener { pulsar() }
     }
 }
