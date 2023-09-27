011e5db41f05231a08b8699c3382a6baf5377180
LuciaES24 <lespsan543@g.educaand.es>
2023-09-27 23:07:29 +0200

    Segunda versión con lateinit

--- app/src/main/java/com/lespsan543/ejercicio1pmdm/MainActivity.kt
+++ app/src/main/java/com/lespsan543/ejercicio1pmdm/MainActivity.kt
@@ -40,11 +40,42 @@ class MainActivity : AppCompatActivity() {
                 toast.show()
             }
         }
+        /**
+        //Segunda versión con variables de clase y lateinit
+        var contaPulsaciones = 0
+        lateinit var botonLateInit : Button
+        lateinit var textLateInit : TextView
 
         fun pulsarLateInit(){
+            botonLateInit = findViewById(R.id.boton_aceptar)
+            textLateInit = findViewById(R.id.textoMostrar)
 
+            contaPulsaciones++
+            var tamano = 0
+            if (contaPulsaciones<=5){
+                tamano = 24
+            }else{
+                tamano = 22
+            }
+            textLateInit.textSize = tamano.toFloat()
+
+            if (contaPulsaciones == 1){
+                textLateInit.text = "Has hecho click una vez!"
+            }else if(contaPulsaciones == 2){
+                textLateInit.text = "Has hecho click dos veces!"
+            }else if(contaPulsaciones>=3 && contaPulsaciones<=5){
+                textLateInit.text = "Has hecho click $contaPulsaciones veces!"
+            }else if(contaPulsaciones>=6 && contaPulsaciones <=9){
+                textLateInit.text = "Has hecho click varias veces! ($contaPulsaciones)"
+            }else {
+                textLateInit.text = "Te has pasado de clicks!"
+                botonLateInit.setEnabled(false)
+                val toast = Toast.makeText(this,"El botón ha sido deshabilitado",Toast.LENGTH_SHORT)
+                toast.show()
+            }
         }
-
+        **/
         boton.setOnClickListener { pulsar() }
+        //botonLateInit.setOnClickListener { pulsarLateInit() }
     }
 }
